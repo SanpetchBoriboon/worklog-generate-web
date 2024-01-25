5faa6f176e504d21eb7bbf83126fbea113139173
Sanpetch_ben <sunpetch1997@gmail.com>
2024-01-25 14:59:48 +0700

    Create generate page

--- src/app/page.js
+++ src/app/page.js
@@ -7,28 +7,45 @@ export default function Home() {
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
+
 		if (!file) return;
 
 		try {
 			const data = new FormData();
-			data.set("file", file);
+			data.append("file", file);
 
-			console.log("file", file);
+			const fileName = file.name
+
+			const yaerMonth = fileName.split('_')[1].split('-');
+      		const generateFileName = `Report JiraWorkLog - ${yaerMonth[0]}_${yaerMonth[1]}.xlsx`;
 
 			const res = await fetch("http://localhost:3001/worklogFile/generateFile", {
 				method: "POST",
-				headers: {
-					"Content-Type": "multipart/form-data; boundary=<calculated when request is sent>",
-				},
-				body: { file: data },
+				body: data,
 			});
 
-      console.log('res',res)
+			if (!res.ok) {
+				throw new Error(await res.text());
+			}
 
-			// handle the error
-			if (!res.ok) throw new Error(await res.text());
+			// // Assuming the response is a Blob containing the Excel file
+			const blob = await res.blob();
+
+			// Create a temporary link to trigger the download
+			const link = document.createElement("a");
+			link.href = window.URL.createObjectURL(blob);
+			link.download = generateFileName;
+			document.body.appendChild(link);
+
+			// Trigger the download
+			link.click();
+
+			// Remove the temporary link
+			document.body.removeChild(link);
+
+			// Reload the window after download
+			window.location.reload();
 		} catch (e) {
-			// Handle errors here
 			console.error(e);
 		}
 	};
