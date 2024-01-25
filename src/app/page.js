3afb1ce92223d0d00b3f215d3ba5a496f12298d7
Sanpetch_ben <sunpetch1997@gmail.com>
2024-01-25 20:07:37 +0700

    add conditon for upload button

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@
 import { useState } from "react";
 
 export default function Home() {
-	const [file, setFile] = useState();
+	const [file, setFile] = useState(null);
+	const [loading, setLoading] = useState(false);
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
@@ -11,15 +12,16 @@ export default function Home() {
 		if (!file) return;
 
 		try {
+			setLoading(true);
+
 			const data = new FormData();
 			data.append("file", file);
 
 			const fileName = file.name;
-
 			const yaerMonth = fileName.split("_")[1].split("-");
 			const generateFileName = `Report JiraWorkLog - ${yaerMonth[0]}_${yaerMonth[1]}.xlsx`;
 
-			const res = await fetch("https://generate-worklog-server.fly.dev/worklogFile/generateFile", {
+			const res = await fetch(`https://generate-worklog-server.fly.dev/worklogFile/generateFile`, {
 				method: "POST",
 				body: data,
 			});
@@ -28,25 +30,20 @@ export default function Home() {
 				throw new Error(await res.text());
 			}
 
-			// // Assuming the response is a Blob containing the Excel file
 			const blob = await res.blob();
-
-			// Create a temporary link to trigger the download
 			const link = document.createElement("a");
 			link.href = window.URL.createObjectURL(blob);
 			link.download = generateFileName;
 			document.body.appendChild(link);
-
-			// Trigger the download
 			link.click();
-
-			// Remove the temporary link
 			document.body.removeChild(link);
 
 			// Reload the window after download
 			window.location.reload();
 		} catch (e) {
 			console.error(e);
+		} finally {
+			setLoading(false);
 		}
 	};
 
@@ -61,7 +58,6 @@ export default function Home() {
 								<label className="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
 									<div className="flex flex-col items-center justify-center pt-7">
 										{file ? (
-											// Display a different SVG when a file is selected
 											<svg
 												xmlns="http://www.w3.org/2000/svg"
 												className="w-8 h-8 text-green-400 group-hover:text-green-600"
@@ -72,7 +68,6 @@ export default function Home() {
 												<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
 											</svg>
 										) : (
-											// Default SVG when no file is selected
 											<svg
 												xmlns="http://www.w3.org/2000/svg"
 												className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
@@ -97,8 +92,12 @@ export default function Home() {
 							</div>
 						</div>
 						<div className="flex justify-center p-2">
-							<button className="w-full px-4 py-2 text-white bg-blue-500 rounded shadow-xl" type="submit">
-								Submit
+							<button
+								className={`w-full px-4 py-2 text-white ${file && !loading ? "bg-green-500" : "bg-gray-500"} rounded shadow-x`}
+								type="submit"
+								disabled={!file || loading}
+							>
+								{loading ? "Uploading..." : "Upload"}
 							</button>
 						</div>
 					</div>
